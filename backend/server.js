cf0ed85b3a383785af15e48e2239fe98bf8ab641
Keerthana R <kitcbe.25.21bcs022@gmail.com>
2025-08-27 20:12:30 +0530

    added

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/recipeDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Recipe model
+const RecipeSchema = new mongoose.Schema({
+  name: String,
+  ingredients: String,
+  instructions: String,
+});
+const Recipe = mongoose.model("Recipe", RecipeSchema);
+
+// Routes
+app.get("/recipes", async (req, res) => {
+  const recipes = await Recipe.find();
+  res.json(recipes);
+});
+
+app.post("/recipes", async (req, res) => {
+  const recipe = new Recipe(req.body);
+  await recipe.save();
+  res.json(recipe);
+});
+
+app.delete("/recipes/:id", async (req, res) => {
+  await Recipe.findByIdAndDelete(req.params.id);
+  res.json({ message: "Recipe deleted" });
+});
+
+// Start server
+app.listen(5000, () => console.log("Server running on port 5000"));
