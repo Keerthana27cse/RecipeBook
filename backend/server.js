9d5591eb3773c9a29fa49201479aab56723e1046
Keerthana R <kitcbe.25.21bcs022@gmail.com>
2025-08-27 21:01:08 +0530

    seconds

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,13 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/recipeDB", {
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
 // Recipe model
 const RecipeSchema = new mongoose.Schema({
   name: String,
