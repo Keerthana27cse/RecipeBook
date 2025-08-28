f8b27a2860d9ee910da652c920b62ab7acf052a2
Keerthana R <kitcbe.25.21bcs022@gmail.com>
2025-08-28 12:23:55 +0530

    third

--- backend/server.js
+++ backend/server.js
@@ -45,4 +45,4 @@ app.delete("/recipes/:id", async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(3000, () => console.log("Server running on port 3000"));
